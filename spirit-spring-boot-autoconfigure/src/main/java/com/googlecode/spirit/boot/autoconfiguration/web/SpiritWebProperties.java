b44ba04caca4788b9a5cb0f3233f7c89d865466a
JunJie <cerci.jj@gmail.com>
2018-01-10 13:25:07 +0800

    更新spirit web的属性定义

--- spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/web/SpiritWebProperties.java
+++ spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/web/SpiritWebProperties.java
@@ -8,10 +8,46 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spirit.web")
 public class SpiritWebProperties {
 
+    private boolean useHttpRequest;
+    private boolean useStringStream;
+    private boolean useJsonStream;
+    private boolean useJaxbStream;
     private int defaultMaxPerRoute;
     private int maxTotal;
     private String jsonDateFormat;
     private String jaxbPackageToScan;
+
+    public boolean isUseHttpRequest() {
+        return useHttpRequest;
+    }
+
+    public void setUseHttpRequest(boolean useHttpRequest) {
+        this.useHttpRequest = useHttpRequest;
+    }
+
+    public boolean isUseStringStream() {
+        return useStringStream;
+    }
+
+    public void setUseStringStream(boolean useStringStream) {
+        this.useStringStream = useStringStream;
+    }
+
+    public boolean isUseJsonStream() {
+        return useJsonStream;
+    }
+
+    public void setUseJsonStream(boolean useJsonStream) {
+        this.useJsonStream = useJsonStream;
+    }
+
+    public boolean isUseJaxbStream() {
+        return useJaxbStream;
+    }
+
+    public void setUseJaxbStream(boolean useJaxbStream) {
+        this.useJaxbStream = useJaxbStream;
+    }
 
     public int getDefaultMaxPerRoute() {
         return defaultMaxPerRoute;
