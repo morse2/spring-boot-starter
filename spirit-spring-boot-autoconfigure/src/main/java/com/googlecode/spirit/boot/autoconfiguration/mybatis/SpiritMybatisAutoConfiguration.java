9373dbea32e88b758d90b29330f38a48f047f460
JunJie.Z <cerci.jj@gmail.com>
2019-05-30 13:09:35 +0800

    1.为MyBatis SqlSessionFactory定制属性 2.去掉了mybatis spring boot的依赖

--- spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/mybatis/SpiritMybatisAutoConfiguration.java
+++ spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/mybatis/SpiritMybatisAutoConfiguration.java
@@ -13,31 +13,135 @@ import com.googlecode.easyec.spirit.mybatis.paging.support.MybatisPagingIntercep
 import com.googlecode.easyec.spirit.mybatis.service.impl.DelegateServiceBeanPostProcessor;
 import com.googlecode.easyec.spirit.web.controller.formbean.impl.AbstractSearchFormBean;
 import com.googlecode.spirit.boot.autoconfiguration.dao.SpiritDaoProperties;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
-import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Stream;
 
 /**
  * Created by JunJie on 11/18/16.
  */
-@Configuration
-@ConditionalOnBean({ SqlSessionFactory.class })
+@org.springframework.context.annotation.Configuration
+@ConditionalOnClass({ SqlSessionFactory.class, SqlSessionFactoryBean.class })
+@ConditionalOnBean(DataSource.class)
 @EnableConfigurationProperties({ SpiritMybatisProperties.class, SpiritDaoProperties.class })
-@AutoConfigureAfter(MybatisAutoConfiguration.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
 public class SpiritMybatisAutoConfiguration {
 
-    @Resource
+    private static final Logger logger = LoggerFactory.getLogger(SpiritMybatisAutoConfiguration.class);
+
     private SpiritMybatisProperties spiritMybatisProperties;
-    @Resource
     private SpiritDaoProperties spiritDaoProperties;
+
+    private ResourcePatternResolver resourcePatternResolver;
+    private final Interceptor[] interceptors;
+    private final DatabaseIdProvider databaseIdProvider;
+
+    public SpiritMybatisAutoConfiguration(
+        SpiritDaoProperties spiritDaoProperties,
+        SpiritMybatisProperties spiritMybatisProperties,
+        ResourcePatternResolver resourcePatternResolver,
+        ObjectProvider<Interceptor[]> interceptorsProvider,
+        ObjectProvider<DatabaseIdProvider> databaseIdProvider) {
+        this.spiritDaoProperties = spiritDaoProperties;
+        this.spiritMybatisProperties = spiritMybatisProperties;
+        this.resourcePatternResolver = resourcePatternResolver;
+        this.interceptors = interceptorsProvider.getIfAvailable();
+        this.databaseIdProvider = databaseIdProvider.getIfAvailable();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+
+        String configLocation = spiritMybatisProperties.getConfigLocation();
+        if (StringUtils.hasText(configLocation)) {
+            factory.setConfigLocation(resourcePatternResolver.getResource(configLocation));
+        }
+
+        org.apache.ibatis.session.Configuration configuration = spiritMybatisProperties.getConfiguration();
+        if (configuration == null && !StringUtils.hasText(configLocation)) {
+            configuration = new org.apache.ibatis.session.Configuration();
+        }
+
+        factory.setConfiguration(configuration);
+
+        final Properties properties = new Properties();
+        Resource[] propertiesResources = getResources(spiritMybatisProperties.getConfigurationPropertiesLocations());
+        if (!ObjectUtils.isEmpty(propertiesResources)) {
+            Stream.of(propertiesResources).forEach(res -> {
+                try {
+                    properties.load(res.getInputStream());
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                    // ignore..
+                }
+            });
+        }
+
+        Properties configurationProperties = spiritMybatisProperties.getConfigurationProperties();
+        if (configurationProperties != null) {
+            properties.putAll(configurationProperties);
+        }
+
+        if (!properties.isEmpty()) {
+            factory.setConfigurationProperties(properties);
+        }
+
+        if (!ObjectUtils.isEmpty(this.interceptors)) {
+            factory.setPlugins(this.interceptors);
+        }
+
+        if (this.databaseIdProvider != null) {
+            factory.setDatabaseIdProvider(this.databaseIdProvider);
+        }
+        String typeAliasesPackage = spiritMybatisProperties.getTypeAliasesPackage();
+        if (StringUtils.hasLength(typeAliasesPackage)) {
+            factory.setTypeAliasesPackage(typeAliasesPackage);
+        }
+        if (StringUtils.hasLength(spiritMybatisProperties.getTypeHandlersPackage())) {
+            factory.setTypeHandlersPackage(spiritMybatisProperties.getTypeHandlersPackage());
+        }
+
+        Resource[] mapperLocations = getResources(spiritMybatisProperties.getMapperLocations());
+        if (!ObjectUtils.isEmpty(mapperLocations)) {
+            factory.setMapperLocations(mapperLocations);
+        }
+
+        return factory.getObject();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 
     @Bean
     @ConditionalOnBean(PageDialect.class)
@@ -80,6 +184,23 @@ public class SpiritMybatisAutoConfiguration {
         interceptor.setSqlSessionFactory(sqlSessionFactory);
 
         return interceptor;
+    }
+
+    private Resource[] getResources(String[] locations) {
+        if (ObjectUtils.isEmpty(locations)) return new Resource[0];
+
+        List<Resource> result = new ArrayList<>();
+        for (String location : locations) {
+            try {
+                Resource[] resources = resourcePatternResolver.getResources(location);
+                result.addAll(Arrays.asList(resources));
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+                // ignore..
+            }
+        }
+
+        return result.toArray(new Resource[0]);
     }
 
     private class InternalMybatisPageDelegate extends PageDelegate<MybatisPage> {
--- spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/mybatis/SpiritMybatisProperties.java
+++ spirit-spring-boot-autoconfigure/src/main/java/com/googlecode/spirit/boot/autoconfiguration/mybatis/SpiritMybatisProperties.java
@@ -1,6 +1,10 @@
 package com.googlecode.spirit.boot.autoconfiguration.mybatis;
 
+import org.apache.ibatis.session.Configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+import java.util.Properties;
 
 /**
  * Created by JunJie on 11/18/16.
@@ -13,6 +17,38 @@ public class SpiritMybatisProperties {
     private int pagingInterceptOrder = 4;
     private boolean usePagingInterceptor = true;
     private boolean usePageDelegate;
+
+    /* mybatis properties */
+    /**
+     * Location of MyBatis xml config file.
+     */
+    private String configLocation;
+    /**
+     * A Configuration object for customize default settings. If {@link #configLocation}
+     * is specified, this property is not used.
+     */
+    @NestedConfigurationProperty
+    private Configuration configuration;
+
+    /**
+     * Externalized properties for MyBatis configuration.
+     */
+    private Properties configurationProperties;
+    /**
+     * Locations of MyBatis mapper files.
+     */
+    private String[] mapperLocations;
+
+    /**
+     * Packages to search type aliases. (Package delimiters are ",; \t\n")
+     */
+    private String typeAliasesPackage;
+
+    /**
+     * Packages to search for type handlers. (Package delimiters are ",; \t\n")
+     */
+    private String typeHandlersPackage;
+    private String[] configurationPropertiesLocations;
 
     public int getSqlSessionTemplateDecisionOrder() {
         return sqlSessionTemplateDecisionOrder;
@@ -52,5 +88,61 @@ public class SpiritMybatisProperties {
 
     public void setUsePageDelegate(boolean usePageDelegate) {
         this.usePageDelegate = usePageDelegate;
+    }
+
+    public String getConfigLocation() {
+        return configLocation;
+    }
+
+    public void setConfigLocation(String configLocation) {
+        this.configLocation = configLocation;
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public Properties getConfigurationProperties() {
+        return configurationProperties;
+    }
+
+    public void setConfigurationProperties(Properties configurationProperties) {
+        this.configurationProperties = configurationProperties;
+    }
+
+    public String[] getMapperLocations() {
+        return mapperLocations;
+    }
+
+    public void setMapperLocations(String[] mapperLocations) {
+        this.mapperLocations = mapperLocations;
+    }
+
+    public String getTypeAliasesPackage() {
+        return typeAliasesPackage;
+    }
+
+    public void setTypeAliasesPackage(String typeAliasesPackage) {
+        this.typeAliasesPackage = typeAliasesPackage;
+    }
+
+    public String getTypeHandlersPackage() {
+        return typeHandlersPackage;
+    }
+
+    public void setTypeHandlersPackage(String typeHandlersPackage) {
+        this.typeHandlersPackage = typeHandlersPackage;
+    }
+
+    public String[] getConfigurationPropertiesLocations() {
+        return configurationPropertiesLocations;
+    }
+
+    public void setConfigurationPropertiesLocations(String[] configurationPropertiesLocations) {
+        this.configurationPropertiesLocations = configurationPropertiesLocations;
     }
 }
